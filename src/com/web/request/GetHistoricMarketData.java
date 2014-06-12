30b63ffc7acaccb92c3cf6725de87aa941f63c6c
BenBergCapital <ben512@hotmail.co.uk>
2014-06-12 21:26:52 +0100

    Added WebListener for chart data

--- src/com/web/request/GetHistoricMarketData.java
+++ src/com/web/request/GetHistoricMarketData.java
@@ -13,32 +13,78 @@ import com.ib.sample.IBTradingMain;
 
 
 public class GetHistoricMarketData {
+	
+	private static GetHistoricMarketData instance = null;
+	HashMap<Integer, IHistoricalDataHandler> MarketDataMap;
+	ApiController m_controller;
+	IBTradingMain  IBMain;
+	public GetHistoricMarketData()
+	{
+		 IBMain = IBTradingMain.INSTANCE;
+		m_controller = IBMain.GetController();
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	 public static GetHistoricMarketData getInstance() {
+	      if(instance == null) {
+	         instance = new GetHistoricMarketData();
+	      }
+	      return instance;
+	   }
 
-	/**
-	 * @param args
-	 */
-	public void get() {
-		// TODO Auto-generated method stub
+	
+	private boolean IsTickerInMap(String Ticker)
+	{
+		
+		MarketDataMap = m_controller.GetHistoricalMap();
+		
+		if(MarketDataMap.containsKey(Ticker))
+			return true;
+		else
+			return false;
+		
+		
+		
+	}
+	
+	
+	public String get(String Ticker) {
+		
+		if(IsTickerInMap(Ticker))
+		{
+		 MarketDataMap.get(Ticker);	
+		}
+		else
+		{	
 
-		IBTradingMain  IBMain = IBTradingMain.INSTANCE;
-		ApiController m_controller = IBMain.GetController();
 		
 		NewContract m_contract = new NewContract();
-		m_contract.symbol( "IBM" ); 
+		m_contract.symbol( Ticker); 
 		m_contract.secType( SecType.STK ); 
 		m_contract.exchange( "SMART" ); 
 		m_contract.currency( "USD" ); 
 				
 		HistoricResultSet dataSet = new HistoricResultSet();
-		IBTradingMain.INSTANCE.controller().reqHistoricalData(m_contract, "20140611 21:00", 1, DurationUnit.DAY, BarSize._10_mins, WhatToShow.TRADES, false, dataSet);
+		IBTradingMain.INSTANCE.controller().reqHistoricalData(m_contract, "20140612 21:00", 1, DurationUnit.SECOND, BarSize._10_mins, WhatToShow.TRADES, false, dataSet);
 //		m_resultsPanel.addTab( "Historical " + m_contract.symbol(), panel, true, true);
+		MarketDataMap = m_controller.GetHistoricalMap();
 		
 		
-		HashMap<Integer, IHistoricalDataHandler> Test = m_controller.GetHistoricalMap();
+		
+		
+		
 		
 		System.out.println(Test.size());
+		}
 		
 		
+		return null;
 	}
 
 }
--- src/com/web/request/ListenForWebRequests.java
+++ src/com/web/request/ListenForWebRequests.java
@@ -1,13 +1,69 @@
 package com.web.request;
 
+import java.io.IOException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+
 public class ListenForWebRequests {
 
-	/**
-	 * @param args
-	 */
+	 private final static String QUEUE_NAME = "q_web_receive";
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		try{
+		    ConnectionFactory factory = new ConnectionFactory();
+		    factory.setHost("localhost");
+		    Connection connection = factory.newConnection();
+		    Channel channel = connection.createChannel();
+
+		    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		    System.out.println(" [*] Waiting for messages on queue "+QUEUE_NAME);
+		    
+		    QueueingConsumer consumer = new QueueingConsumer(channel);
+		    channel.basicConsume(QUEUE_NAME, true, consumer);
+		    
+		    while (true) {
+		      QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+		      String message = new String(delivery.getBody());
+		      
+		      System.out.println(" [x] Received '" + message + "'");
+		      
+		      GetHistoricMarketData MDM = new  GetHistoricMarketData();
+		      MDM = GetHistoricMarketData.getInstance();
+		      SendReplyMessage (MDM.get(message));
+		      
+		    }
+		  }
+		catch (Exception e)
+		{
+			
+			
+		}
+	
 
 	}
 
+	
+	private static void SendReplyMessage(String message) throws IOException
+	{
+		ConnectionFactory factory = new ConnectionFactory();
+	    factory.setHost("localhost");
+	    Connection connection = factory.newConnection();
+	    Channel channel = connection.createChannel();
+
+	    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+	    
+	    channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+	    System.out.println(" [x] Sent '" + message + "'");
+	    
+	    channel.close();
+	    connection.close();
+	  
+		
+		
+	
+	}
+	
 }
