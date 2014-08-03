b48f4e42e4a864bfb230241e7877d4175b4f1853
BenBergCapital <ben512@hotmail.co.uk>
2014-08-03 20:51:53 +0100

    merged

--- src/hft/main/LivePositionHandler.java
+++ src/hft/main/LivePositionHandler.java
@@ -85,17 +85,14 @@ public class LivePositionHandler extends Thread{
 		
 		for (OrderRow row : Cache.instance.GetOpenOrders().m_orders)
 		{
-<<<<<<< HEAD
+
 			log.info("Checking order ticker :  "+ row.m_contract.symbol()+" for position  "+Ticker);	
 			log.info("Order state is : "+row.m_state.status());
 			OrderStatus state = row.m_state.status();
 			if (state ==OrderStatus.Submitted || state==OrderStatus.PreSubmitted)
 			{
-=======
-			if (row.m_state.status() ==OrderStatus.Submitted)
-			{
-			log.info("Checking if close order exists for "+ row.m_contract.symbol());	
->>>>>>> 323a762996a36211c9570c6fe7b7fbe7a5f6b876
+
+
 			if(row.m_contract.symbol().equals(Ticker))
 			{//order pending already
 				//check quantity on order
