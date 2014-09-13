9a812ef06c7646bfdff78028acbff3c05d0a075f
BenBergCapital <ben512@hotmail.co.uk>
2014-09-13 19:52:39 +0100

    amended Bar

--- src/com/ib/controller/Bar.java
+++ src/com/ib/controller/Bar.java
@@ -3,17 +3,18 @@
 
 package com.ib.controller;
 
+import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Bar {
+public class Bar  implements Serializable{
 	private static final SimpleDateFormat FORMAT = new SimpleDateFormat( "yyyyMMdd HH:mm:ss"); // format for historical query
 
-	private final long m_time;
-	private final double m_high;
-	private final double m_low;
-	private final double m_open;
-	private final double m_close;
+	private long m_time;
+	private double m_high;
+	private double m_low;
+	private double m_open;
+	private double m_close;
 	private final double m_wap;
 	private final long m_volume;
 	private final int m_count;
@@ -37,7 +38,17 @@ public class Bar {
 		m_volume = volume;
 		m_count = count;
 	}
-
+	public Bar()
+	{
+		m_time = 0;
+		m_high = 0;
+		m_low = 0;
+		m_open = 0;
+		m_close = 0;
+		m_wap = 0;
+		m_volume =0;
+		m_count = 0;
+	}
 	public String formattedTime() {
 		return Formats.fmtDate( m_time * 1000);
 	}
@@ -47,7 +58,37 @@ public class Bar {
 		return FORMAT.format( new Date( ms) );
 	}
 
+	public void SetHigh(double value)
+	{
+		m_high=value;
+	}
+	public void SetLow(double value)
+	{
+		m_low=value;
+	}
+	public void SetOpen(double value)
+	{
+		m_open=value;
+	}
+	public void SetClose(double value)
+	{
+		m_close=value;
+	}
+	public void SetTime(long value)
+	{
+		m_time=value;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	@Override public String toString() {
 		return String.format( "%s %s %s %s %s", formattedTime(), m_open, m_high, m_low, m_close);
 	}
+	
 }
